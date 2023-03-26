337591dfa43e15aa6c52e2e9659144c919345d00
Brad Hughes <brad94901@gmail.com>
2023-03-25 20:11:25 -0500

    created project files

---
+++ Project2.cpp
@@ -0,0 +1,14 @@
+/*
+Project 2
+Brad Hughes, Zohair Khan
+Implementation of the A* algorithm on the 8-square puzzle
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(){
+	
+
+}
---
+++ Project2.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
+#include <vector>
+
+
+//Bunch of constant and object declarations.
